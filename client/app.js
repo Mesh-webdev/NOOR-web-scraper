74fb5840bf2335a6ba678a6c7d3016232ff8319b
Meshari <msharisul696@gmail.com>
2020-04-05 13:58:34 +0300

    Worked on the front-end. added a dark/light mode toggler. 50% done

--- client/app.js
+++ client/app.js
@@ -1,9 +1,14 @@
+const body = document.body;
 const username = document.getElementById('username');
 const pwd = document.getElementById('password');
 const captcha = document.getElementById('captcha');
 const captchaId = document.getElementById('captchaId');
 const loginBtn = document.getElementById('login');
 const scrapeBtn = document.getElementById('scrape');
+const themeToggleBtn = document.getElementById('theme-toggle');
+
+//$('.form').hide();
+$('#scrape').hide();
 
 let captchaInput;
 
@@ -11,6 +16,7 @@ let captchaInput;
 const socket = io.connect('http://localhost:4000');
 
 scrapeBtn.addEventListener('click', scrape);
+themeToggleBtn.addEventListener('click', themeToggle);
 
 function scrape() {
   console.log('Scraping...');
@@ -21,19 +27,12 @@ function scrape() {
   socket.emit('scrape', 'New scrape call', (data) => {
     if (data.status) {
       // Get the instance ID from the scrape callback object(data)
-      const instance = data.instance;
-      const imgUrl = '../server/images/' + instance + '.png';
-      console.log(path);
-      // Create the DOM img element
-      var imgNode = document.createElement('img');
-      imgNode.setAttribute('href', imgUrl);
-
-      captchaId.appendChild(imgNode);
-
-      captchaId.append();
-
       $('#loader').fadeOut(100, () => {
-        $('#form').fadeIn();
+        // Should show that the script has finished and captcha is found
+        // with a check mark
+        // then show the form
+        // better ux
+        $('.form').fadeIn();
       });
 
       loginBtn.addEventListener('click', login);
@@ -48,4 +47,18 @@ function scrape() {
   // call scrape from back-end
   // after its done, hide loader
   // show form with the captcha from the back-end(?? YIKES)
+}
+
+function themeToggle() {
+
+  if ($('body').attr('class') == 'light') {
+    themeToggleBtn.value = 'Light mode'
+    $('body').removeClass('light')
+    $('body').addClass('dark')
+  } else {
+    themeToggleBtn.value = 'Dark mode'
+    $('body').removeClass('dark')
+    $('body').addClass('light')
+  }
+
 }
